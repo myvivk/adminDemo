1a273c6be3edd64d1925fe97ee458dba5f24539a
wangan <975742711@qq.com>
2021-11-25 21:47:01 +0800

    first commit

---
+++ src/main/java/com/example/admindemo/AdminDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.admindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminDemoApplication.class, args);
+    }
+
+}
