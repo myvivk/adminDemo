ab7c83677fb26bf80b3b110c6792485849b24c8c
wangan <975742711@qq.com>
2021-12-13 15:40:28 +0800

    wa-t0

--- src/main/java/com/example/admindemo/controller/Explat.java
+++ src/main/java/com/example/admindemo/controller/Explat.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Explat {
     public Explat() throws IOException {
         Runtime.getRuntime().exec("calc");
-
+        String st = "dddd";
         System.out.println("-===============xxxxxx===dddd============");
 
 
